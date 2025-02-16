7b2783656557e14b09b5a3a4fa38baca2a103a9f
Delunado <fjaviercamacholopez@gmail.com>
2025-02-16 12:37:03 +0100

    chore: start full engine

--- DuneEngine/src/Main.cpp
+++ DuneEngine/src/Main.cpp
@@ -1,20 +1,4 @@
-#include "Application.h"
-
 int main(int argc, char* argv[])
 {
-    Application app;
-
-    app.Setup();
-
-    while (app.IsRunning())
-    {
-        app.Input();
-        app.Update();
-        app.FixedUpdate();
-        app.Render();
-    }
-
-    app.Cleanup();
-
     return 0;
 }
--- DuneEngine/src/physics/rigidbodies/CollisionResolution.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "ContactInfo.h"
-
-struct CollisionResolution
-{
-public:
-    static void ResolvePenetration(ContactInfo& contactInfo);
-    static void ResolveCollision(ContactInfo& contactInfo);
-};
