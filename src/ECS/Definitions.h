240ef6c71e7277584d2c55dd494b5a18a2a806df
Delunado <fjaviercamacholopez@gmail.com>
2025-02-23 19:25:00 +0100

    feat: create base System data

---
+++ src/ECS/Definitions.h
@@ -0,0 +1,8 @@
+﻿#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+#include <bitset>
+
+constexpr unsigned int MAX_COMPONENTS = 32;
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
+#endif //DEFINITIONS_H
--- src/ECS/System.h
+++ src/ECS/System.h
@@ -1,16 +1,31 @@
-﻿//
-// Created by fjavi on 23/02/2025.
-//
-
-#ifndef SYSTEM_H
+﻿#ifndef SYSTEM_H
 #define SYSTEM_H
+#include <bitset>
+#include <vector>
 
+#include "Definitions.h"
 
+class Entity;
 
 class System {
+public:
+    System() = default;
 
+    ~System() = default;
+
+    void AddEntity(const Entity &entity);
+
+    void RemoveEntity(const Entity &entity);
+
+    std::vector<Entity> GetEntities() const;
+
+    Signature& GetSignature() const;
+
+private:
+    Signature _componentSignature;
+
+    std::vector<Entity> _entities;
 };
-
 
 
 #endif //SYSTEM_H
