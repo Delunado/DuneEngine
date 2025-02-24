a44429f5bc8e2b48be3e6712b23826203cff203b
Delunado <fjaviercamacholopez@gmail.com>
2025-02-24 18:36:38 +0100

    feat: create base Component data

--- src/ECS/Component.h
+++ src/ECS/Component.h
@@ -1,12 +1,18 @@
 ﻿#ifndef COMPONENT_H
 #define COMPONENT_H
 
-
-
-class Component {
-
+struct IComponent {
+protected:
+    static int nextId;
 };
 
-
+template<typename T>
+class Component : public IComponent {
+public:
+    static int GetId() {
+        static int id = nextId++;
+        return id;
+    }
+};
 
 #endif //COMPONENT_H
