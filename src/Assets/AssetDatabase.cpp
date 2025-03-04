ea955832ca38ddd6fc7f58bbf8d14157a0ce4396
Delunado <fjaviercamacholopez@gmail.com>
2025-03-04 19:37:02 +0100

    feat: create asset database

---
+++ src/Assets/AssetDatabase.cpp
@@ -0,0 +1,32 @@
+﻿#include "AssetDatabase.h"
+
+#include <Logger.h>
+#include <SDL_image.h>
+
+AssetDatabase::AssetDatabase() {
+    Logger::Log("AssetDatabase created");
+}
+
+AssetDatabase::~AssetDatabase() {
+    Logger::Log("AssetDatabase destroyed");
+}
+
+void AssetDatabase::AddTexture(SDL_Renderer *renderer, const std::string &id, const std::string &filePath) {
+    SDL_Surface *surface = IMG_Load(filePath.c_str());
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    _textures.emplace(id, texture);
+
+    SDL_FreeSurface(surface);
+}
+
+SDL_Texture *AssetDatabase::GetTexture(const std::string &id) const {
+    return _textures.at(id);
+}
+
+void AssetDatabase::Clear() {
+    for (const auto &[id, texture]: _textures) {
+        SDL_DestroyTexture(texture);
+    }
+
+    _textures.clear();
+}
---
+++ src/Assets/AssetDatabase.h
@@ -0,0 +1,24 @@
+﻿#ifndef ASSETDATABASE_H
+#define ASSETDATABASE_H
+
+#include <map>
+#include <string>
+#include <SDL.h>
+
+class AssetDatabase {
+public:
+    AssetDatabase();
+
+    ~AssetDatabase();
+
+    void AddTexture(SDL_Renderer *renderer, const std::string &id, const std::string &filePath);
+
+    SDL_Texture *GetTexture(const std::string &id) const;
+
+    void Clear();
+
+private:
+    std::map<std::string, SDL_Texture *> _textures;
+};
+
+#endif //ASSETDATABASE_H
